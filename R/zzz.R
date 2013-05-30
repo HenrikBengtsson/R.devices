507e8c9853dc84f1edb43a7813f7bee37e7ec439
Henrik Bengtsson <hb@aroma-project.org>
2013-05-30 22:38:39 +0000

    Version: 2.3.0 [2013-05-30] o Now the package vignette is built via the R (>= 3.0.0) vignette engines.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/r-dots/pkg/R.devices@1442 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/zzz.R
+++ R/zzz.R
@@ -2,9 +2,23 @@
 # conflicts() in [R] base.
 .conflicts.OK <- TRUE;
 
+.onLoad <- function(libname, pkgname) {
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Register vignette engines
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  try({
+    vignetteEngine <- get("vignetteEngine", envir=asNamespace("tools"));
+
+    # RSP engine
+    vignetteEngine("rsp", package=pkgname, pattern="[.][^.]*[.]rsp$",
+                    weave=R.rsp::rspWeave, tangle=R.rsp::rspTangle);
+  }, silent=TRUE)
+}
+
+
 
 ## .First.lib <- function(libname, pkgname) {
-.onAttach <- function(libname, pkgname) { 
+.onAttach <- function(libname, pkgname) {
   pkg <- Package(pkgname);
   pos <- getPosition(pkg);
   assign(pkgname, pkg, pos=pos);
@@ -12,7 +26,9 @@
 }
 
 ############################################################################
-# HISTORY: 
+# HISTORY:
+# 2013-05-30
+# o Now registering RSP vignette engines.
 # 2010-11-05
 # o Created.
 ############################################################################
