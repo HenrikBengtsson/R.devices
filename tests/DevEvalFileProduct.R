c0230faffbf23e2cdf9f04f7056c481f9f04894a
Henrik Bengtsson <hb@aroma-project.org>
2014-01-12 02:56:52 +0000

    Version: 2.8.4 [2014-01-11] o Added asDataURI() for converting an image file to an   Base64-encoded data URI character string.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/r-dots/pkg/R.devices@1961 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- tests/DevEvalFileProduct.R
+++ tests/DevEvalFileProduct.R
@@ -6,11 +6,7 @@ res <- R.devices::toPNG("foo", tags=c("a", "b"), aspectRatio=0.7, {
 print(res)
 str(res)
 
-fields <- c("name", "fullname", "filename", "pathname")
-if (R.utils::isPackageInstalled("base64enc")) {
-  fields <- c(fields, "dataURI")
-}
-
+fields <- c("name", "fullname", "filename", "pathname", "dataURI")
 for (ff in fields) {
   cat(sprintf("%s: %s\n", ff, substring(res[[ff]], 1, 50)))
 }
--- tests/dataURI.R
+++ tests/dataURI.R
@@ -1,9 +1,14 @@
-if (R.utils::isPackageInstalled("base64enc")) {
-  options("devEval/args/field"="dataURI")
+options("devEval/args/field"="dataURI")
+uri <- R.devices::toPNG("foo", tags=c("a", "b"), aspectRatio=0.7, {
+  plot(1:10)
+})
+str(uri)
 
-  uri <- R.devices::toPNG("foo", tags=c("a", "b"), aspectRatio=0.7, {
-    plot(1:10)
-  })
-  str(uri)
-}
 
+options("devEval/args/field"="pathname")
+png <- R.devices::toPNG("foo", tags=c("a", "b"), aspectRatio=0.7, {
+  plot(1:10)
+}, force=FALSE)
+uri2 <- R.devices::asDataURI(png)
+str(uri2)
+stopifnot(identical(uri2, uri))
