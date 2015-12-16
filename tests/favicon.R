7c9c82e827dee4c389e5be72dfd4b9bfa02a7196
hb <hb@aroma-project.org>
2015-12-15 20:33:59 -0800

    TESTS: Adding tests for all toNnn() functions

--- tests/favicon.R
+++ tests/favicon.R
@@ -11,6 +11,15 @@ dataURI <- toFavicon({
   points(1/2,6/2, pch=21, cex=21, lwd=80, col="#FFFFFF")
 })
 print(dataURI)
+
+pathname <- toFavicon({
+  plot(NA, xlim=c(0,1), ylim=c(0,6), axes=FALSE, xaxs="i", yaxs="i")
+  col <- rev(c("#FF0000", "#FF8000", "#FFFF00", "#008000", "#0000FF", "#A000C0"))
+  for (kk in 1:6) rect(0,kk-1,1,kk, col=col[kk], border=NA)
+  points(1/2,6/2, pch=21, cex=21, lwd=80, col="#FFFFFF")
+}, field="pathname")
+print(pathname)
+
 message("- toFavicon() ... DONE")
 
 
