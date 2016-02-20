6b0c15acd2ab8c13cf336d2cc71f21c7cad83418
hb <hb@aroma-project.org>
2016-02-20 10:10:51 -0800

    example(capturePlot) now illustrated that dimensions are controlled when replaying [#19]

--- tests/capturePlot.R
+++ tests/capturePlot.R
@@ -2,6 +2,9 @@ library("R.devices")
 
 message("*** capturePlot() ...")
 
+cat("Default graphics device:\n")
+str(getOption("device"))
+
 if (getRversion() >= "3.3.0") {
   g <- capturePlot({
     plot(1:10)
@@ -10,8 +13,11 @@ if (getRversion() >= "3.3.0") {
   ## Display
   print(g)
 
+  ## Display with a 2/3 aspect ratio
+##  toDefault(aspectRatio=2/3, print(g))
+
   ## Redraw to many output formats
-  devEval(c("png", "eps", "pdf"), print(g))
+  devEval(c("png", "eps", "pdf"), aspectRatio=2/3, print(g))
 
 } ## if (getRversion() >= "3.3.0")
 
