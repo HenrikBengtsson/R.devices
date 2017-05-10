642a0f106fa272bec166d7729d29b0930251c6f6
hb <hb@aroma-project.org>
2017-05-09 19:06:45 -0700

    Just for completeness: is_nullfile() works for more Windows cases

--- R/utils.R
+++ R/utils.R
@@ -1,10 +1,19 @@
 nullfile <- function() {
   switch(.Platform$OS.type,
     windows = "NUL",
-    "/dev/null"
+    "/dev/null" #nolint
   )
 }
 
 is_nullfile <- function(file) {
-  normalizePath(file, mustWork = FALSE) == nullfile()
+  file <- normalizePath(file, mustWork = FALSE)
+  file <- tolower(file)
+
+  if (.Platform$OS.type == "windows") {
+    # Any filename of NUL, NUL.<any extension>, NUL:<any path>
+    file <- gsub("[.:].*", "", basename(file))
+    (file == "nul")
+  } else {
+    (file == "/dev/null") #nolint
+  }
 }
